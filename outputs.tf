4f64b2b91e81847297fdabcffcba702bff34e6b7
Nikita Bakun <54331408+not-only-yours@users.noreply.github.com>
2025-04-24 20:24:35 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "atlas_cli_output" {
-  description = "Output of the Atlas CLI command."
+  description = "Output of the Atlas CLI command in JSON format."
   value       = jsondecode(data.local_file.this.content).results
 }
 
 output "atlas_cli_output_raw" {
-  description = "Output of the Atlas CLI command."
+  description = "Output of the Atlas CLI command in raw format."
   value       = data.local_file.this.content
 }
