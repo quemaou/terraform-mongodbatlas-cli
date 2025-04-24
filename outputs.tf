6445e226499eb2ec633d1a38a3be0cadf5232d94
Nikita Bakun <54331408+not-only-yours@users.noreply.github.com>
2025-04-24 20:23:57 +0300

    Add row output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "atlas_cli_output" {
   description = "Output of the Atlas CLI command."
   value       = jsondecode(data.local_file.this.content).results
 }
+
+output "atlas_cli_output_raw" {
+  description = "Output of the Atlas CLI command."
+  value       = data.local_file.this.content
+}
