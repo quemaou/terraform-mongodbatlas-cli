7f4cadea6c7d81e0a6d5a3cc1bf3b809b82f7b9d
Nikita Bakun <54331408+not-only-yours@users.noreply.github.com>
2025-04-24 20:51:17 +0300

    Update example.tf

--- examples/example.tf
+++ examples/example.tf
@@ -1,12 +1,5 @@
 terraform {
   required_version = ">= 1.3.7"
-
-  required_providers {
-    mongodbatlas = {
-      source  = "mongodb/mongodbatlas"
-      version = ">= 1.21.4"
-    }
-  }
 }
 
 provider "mongodbatlas" {}
